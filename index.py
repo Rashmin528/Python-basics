60012f2fa3a7065e48e1885ec639d649f19a581d
Rashmin M <rashmingit@gmail.com>
2018-08-14 18:39:26 +0530

    indexing

---
+++ index.py
@@ -0,0 +1,9 @@
+#using index to print
+names = ["India","Italy","turkey"]
+print(names[0])
+print(names[1])
+print(names[2])
+
+items = ["Iran","UK","US"]
+for i,countries in enumerate(items):
+	print(i,countries)
