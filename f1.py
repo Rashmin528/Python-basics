981e58e4f0b105bf9067012f63c8da2e562d2df3
Rashmin M <rashmingit@gmail.com>
2018-09-11 23:21:37 +0530

    functions

---
+++ f1.py
@@ -0,0 +1,15 @@
+#function call
+def add(x,y): #parameters
+	sum = x + y
+	return sum
+
+result = add(4,5) #arguments
+print("answer is" ,result)
+
+
+def add(x,y,z):
+	sum = x+y+z #parameters
+	return sum
+
+result = add(x=2,y=3,z=5) #arguments
+print("answer is",result)
