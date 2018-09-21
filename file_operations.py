0df5d35375ea45251981c5feffa7e9949ad4d3d6
Rashmin M <rashmingit@gmail.com>
2018-09-21 22:23:08 +0530

    basic file operations

---
+++ file_operations.py
@@ -0,0 +1,5 @@
+file = open("hello.txt") #open file
+file = open("hello.txt",'w') #open and write
+file = open("hello.txt",'r') #open and read
+file = open("hello.txt",'r+w') #open read, write both
+file.close() #close opened file
