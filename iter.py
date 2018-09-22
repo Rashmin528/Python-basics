7d884917cbfdf94e49aaa613bec4e49fea83a049
Rashmin M <rashmingit@gmail.com>
2018-09-22 12:21:23 +0530

    iterators or iteration

---
+++ iter.py
@@ -0,0 +1,14 @@
+list = [1,2,3,4] #list of integers
+now = iter(list) #iter the list
+output = next(now) #get the element from now
+print(output) #print
+>>> 1 #output
+output = next(now)
+print(output)
+>>> 2
+output = next(now)
+print(output)
+>>> 3
+output = next(now)
+print(output)
+>>> 4
