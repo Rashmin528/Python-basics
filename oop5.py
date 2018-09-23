f2d48255cdca21d9e9ff90fc5f3b43e8146d361c
Rashmin M <rashmingit@gmail.com>
2018-09-23 15:21:04 +0530

    Add files via upload

---
+++ oop5.py
@@ -0,0 +1,17 @@
+class Pizza:
+	def __init__(self,ingredients):
+		self.ingredients = ingredients
+	
+	@classmethod
+	def mexican(cls, veg):
+		return cls(veg)
+
+	@classmethod
+	def italian(cls, non_veg):
+		return cls(non_veg)
+
+one = Pizza.mexican('onion')
+two = Pizza.italian('chicken')
+print(one.ingredients)
+print(two.ingredients)
+
