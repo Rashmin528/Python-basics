7edc4a1a3bc529a0f5d5c3268680f3c19929b9f8
Rashmin M <rashmingit@gmail.com>
2018-09-23 11:51:23 +0530

    Update oop3.py

--- oop3.py
+++ oop3.py
@@ -1,37 +1,37 @@
-class Person:    
-    Name = ''
+ class Person: #class    
+    Name = ''# class variable
     Age = 0
 
-    def __init__(self, Name, Age):  
-        self.Name = Name  
-        self.Age = Age  
-    
-    def GetName(self):  
+    def __init__(self, Name, Age): #construct  
+        self.Name = Name  #instance variable attri
+        self.Age = Age
+
+    def GetName(self):  #methods
         print(self.Name)
 
     def GetAge(self):
-        print(self.Age)    
-  
-class Student:   
-    Id = ''
-   
-    def __init__(self, Id):  
-        self.Id = Id  
-  
-    def GetId(self):  
-        print(self.Id)  
-  
-  
-class Resident(Person, Student):   
-    one = ''   
+        print(self.Age)
 
-    def __init__(self, Name, Age, Id):  
-        Person.__init__(self, Name, Age)  
-        Student.__init__(self, Id)  
-    
-  
-resident1 = Resident('Rashmin', 22, '2')  
-resident1.GetName()
+class Student: #class
+    Id = '' #class variable
+
+    def __init__(self, Id): #construct  
+        self.Id = Id  #instance variable
+
+    def GetId(self):  #methods
+        print(self.Id)
+
+
+class Resident(Person, Student): #multiple inheritence   
+
+    def __init__(self, Name, Age, Id):
+        Person.__init__(self, Name, Age)
+        Student.__init__(self, Id)
+
+
+resident1 = Resident('Rashmin', 22, '2')  #class instance or object of an instance
+resident1.GetName()#ouput
 resident1.GetAge()
-resident1.GetId()  
+resident1.GetId()
+
 
